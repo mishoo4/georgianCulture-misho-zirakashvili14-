ec27c2d50b8d8e3464b462b83a142f38aadbd351
mishoo4 <104001328+mishoo4@users.noreply.github.com>
2022-07-10 16:06:20 +0400

    Initial Commit

---
+++ georgianCulture(misho zirakashvili14)UITests/georgianCulture_misho_zirakashvili14_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  georgianCulture_misho_zirakashvili14_UITestsLaunchTests.swift
+//  georgianCulture(misho zirakashvili14)UITests
+//
+//  Created by misho zirakashvili on 10.07.22.
+//
+
+import XCTest
+
+class georgianCulture_misho_zirakashvili14_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
